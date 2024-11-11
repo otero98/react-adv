5a444a1226d85743e84a4063a397f994c7214e46
unknown <giovaniotero28@hotmail.com>
2024-11-11 16:09:36 -0600

    Lazyload Basico listo

--- src/01-lazyload/pages/LazyPage2.tsx
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -1,6 +1,9 @@
 
 export const LazyPage2 = () => {
   return (
-   <h1>Lazy Page 2</h1>
+    <h1>Lazy Page 2</h1>
   )
 }
+
+
+export default LazyPage2;
--- src/01-lazyload/pages/LazyPage3.tsx
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -1,6 +1,9 @@
 
 export const LazyPage3 = () => {
   return (
-   <h1>Lazy Page 3</h1>
+    <h1>Lazy Page 3</h1>
   )
 }
+
+
+export default LazyPage3;
